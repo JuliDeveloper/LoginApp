3a0e0cb41d5d534eff8a1a0e9e71c789a1d36604
JuliDeveloper <rja-97@yandex.ru>
2022-04-11 19:45:55 +0300

    Added interface elements and created new view controllers

---
+++ LoginApp/LogInViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LogInViewController.swift
+//  LoginApp
+//
+//  Created by Julia Romanenko on 11.04.2022.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
--- LoginApp/MainViewController.swift
+++ LoginApp/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  LoginApp
 //
 //  Created by Julia Romanenko on 11.04.2022.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+    }
+    
 
 }
-
